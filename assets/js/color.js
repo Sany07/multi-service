b93a51cba34f3a318748abbeb6c734f0d3a0c49e
Ahasan Habib Sany <sany.baust@gmail.com>
2022-09-22 20:42:32 +0600

    done

--- assets/js/color.js
+++ assets/js/color.js
@@ -8,7 +8,6 @@ let colorPalate = document.getElementById('rgb-Color')
 let rgbColorCode = document.getElementById('rgb-color-code')
 let haxColorCode = document.getElementById('hax-color-code')
 let rgbCopyButton = document.getElementById('rgb-copy-button')
-// let haxCopyButton = document.getElementById('hax-copy-button')
 
 
 
@@ -17,9 +16,7 @@ generateColorButton.addEventListener('click', function () {
         if(rgbCopyButton.innerText !='Copy'){
             rgbCopyButton.innerText ='Copy';
         }
-        // if(haxCopyButton.innerText !='Copy'){
-        //     haxCopyButton.innerText ='Copy';
-        // }
+
 
     });
 
@@ -37,20 +34,15 @@ rgbCopyButton.addEventListener('click', function () {
    copyToClipboard(rgbCopyButton,rgbColorCode)
 
 });
-// haxCopyButton.addEventListener('click', function () {
-//    copyToClipboard(haxCopyButton,haxColorCode)
 
-// });
 
 function upDateColor(){
     let rgbCode = generateColor()[0];
     let haxCode = generateColor()[1];
     console.log(rgbCode);
     console.log(haxCode);
-    // colorPalate.style.backgroundColor  = rgbCode;
     document.querySelector('#root').style.backgroundColor  = rgbCode;
     rgbColorCode.value=rgbCode;
-    // haxColorCode.value=haxCode;
     }
 function generateColor(){
     const red = Math.floor(Math.random() * 255);
@@ -58,9 +50,7 @@ function generateColor(){
     const blue = Math.floor(Math.random() * 255);
     const rgbCode = `rgb(${red},${green},${blue})`;
     
-    // convet rgb to hax
-    const haxCode = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 
-    return [rgbCode,haxCode];
+    return [rgbCode];
 }
 
