f98dea305b6b1ad2c32d1c4a2270ed5e33da00e3
Ahasan Habib Sany <sany.baust@gmail.com>
2022-09-22 18:09:21 +0600

    done

---
+++ assets/js/Meal.js
@@ -0,0 +1,67 @@
+window.onload = () => {
+	fetchData('cake')
+};
+
+const btn = document.querySelector('#search-btn')
+let searchItem = document.getElementById('search-box')
+
+btn.addEventListener('click',function(){
+    let data  = searchItem.value;
+    if(data!=''){
+        fetchData(data);
+    }else{
+        alert('Enter Food Nae')
+    }
+})
+
+searchItem.addEventListener("keyup", function(event) {
+
+    let data  = searchItem.value;
+    if (event.key === 'Enter') {
+        fetchData(data);
+    }
+
+});
+
+
+const fetchData = async (name) => {
+
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
+
+    const res = await fetch(url);
+    const data = await res.json();
+    displayResult(data.meals)
+}
+
+const loadMealDetail = async mealId => {
+
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    alert(data.meals[0].strCategory);
+
+}
+
+
+const displayResult = (meals)=>{
+
+    const searchResult = document.getElementById('search-result')
+    searchResult.textContent = '';
+    meals.map(meal => {
+        console.log(meal);
+        const div = document.createElement('div')
+        div.classList.add('col');
+        div.innerHTML = ` 
+        <div class="card">
+          <img src="${meal.strMealThumb}" class="img-fluid card-img-top img" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">
+                <input onclick="loadMealDetail(${meal.idMeal})" id="dt-btn" class="btn btn-info text-white" type="submit" value="Details">
+            </p>
+          </div>
+        </div>`;
+        searchResult.append(div);
+    });
+
+}
